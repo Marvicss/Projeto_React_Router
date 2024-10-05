ffc89c99ccecf0506cb219af84bbcdb841f25124
lucas <bmfmlucas@gmail.com>
2024-10-05 19:08:35 -0300

    feat: added about and home routes

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import { Container, Typography } from '@mui/material'
+import React from 'react'
+
+const About = () => {
+  return (
+    <Container>
+      <Typography variant="h2">Sobre Nós</Typography>
+      <Typography>Informações sobre o projeto.</Typography>
+    </Container>
+  )
+}
+
+export default About
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Typography } from '@mui/material';
+
+function Home() {
+  return (
+   
+      <Container>
+      <Typography variant="h2">Bem-vindo à nossa página Home!</Typography>
+      <Typography>Esta é a página inicial do nosso projeto =D.</Typography>
+    </Container>
+    
+  )
+}
+
+export default Home
