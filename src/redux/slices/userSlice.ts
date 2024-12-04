9c4f4bfca310280048fca09227db36cefab22266
lucas <bmfmlucas@gmail.com>
2024-12-03 21:34:36 -0300

    feat: implemented redux

---
+++ src/redux/slices/userSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface UserState {
+  email: string | null;
+}
+
+const initialState: UserState = {
+  email: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser(state, action: PayloadAction<string>) {
+      state.email = action.payload;
+    },
+    clearUser(state) {
+      state.email = null;
+    },
+  },
+});
+
+export const { setUser, clearUser } = userSlice.actions;
+
+export default userSlice.reducer;
